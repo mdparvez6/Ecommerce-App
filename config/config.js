6e9f35edfcd5da8f7e93184a5fa49239a1c5bd87
mdparvez6 <mdparvez1466@gmail.com>
2024-09-14 16:57:18 +0530

    commit

--- config/config.js
+++ config/config.js
@@ -1,15 +1,26 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const databaseUrl = process.env.DB_URL;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Set to true if you have a valid SSL certificate
+    },
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = sequelize;
